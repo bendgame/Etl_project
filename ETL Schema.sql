84408edd01a8028009d0ef7ff032adf9324f445b
bendgame <44721179+bendgame@users.noreply.github.com>
2019-05-16 13:42:50 -0500

    Add files via upload

---
+++ ETL Schema.sql
@@ -0,0 +1,34 @@
+create database comic_db;
+
+use comic_db;
+
+create table characters(
+character_id int primary key
+,character_name varchar(100)
+,universe varchar(100)
+);
+
+create table char_attributes(
+attribute_id int primary key
+,real_name nvarchar(100)
+,character_id int 
+,universe nvarchar(25)
+,ALIGN nvarchar(25)
+,EYE nvarchar(25)
+,SEX nvarchar(25)
+,Year int
+,foreign key(character_id) REFERENCES characters(character_id)
+);
+
+create table powers(
+power_id int primary key
+,power nvarchar(100)
+);
+
+create table char_powers(
+power_id int
+,character_id int
+,foreign key(power_id) REFERENCES powers(power_id)
+,foreign key(character_id) REFERENCES characters(character_id)
+)
+
